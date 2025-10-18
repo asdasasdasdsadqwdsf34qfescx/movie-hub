66ce0012fd2fdc89723256486ef383d72fae8f57
Sergiu <sergiu.lungu@opentech.email>
2025-10-19 01:40:19 +0300

    chore: remove unused useMemo import from home page

--- src/app/(app)/home/page.tsx
+++ src/app/(app)/home/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { supabase } from "@/services/supabaseClient";
 import Image from "next/image";
